485bded47169e48ff12bbfd3b7ea05a130161194
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-03-26 18:06:07 -0300

    Rotas de casos

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,13 @@
 const { Router } = require('express');
 const routes = Router();
-const crypto = require('crypto');
-const connection = require('./database/connection');
 
-routes.get('/ongs', async (req, res) => {
-  const ongs = await connection.table('ongs').select('*');
-  return res.json(ongs);
-});
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
-routes.post('/ongs', async (req, res) => {
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-  const { name, email, whatsapp, city, uf } = req.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id, name, email, whatsapp, city, uf
-  });
-
-  return res.json({ id });
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;

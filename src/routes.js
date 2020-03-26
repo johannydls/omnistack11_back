ec6e47adbf733e5ffb833cb54f3d01ddc51b6877
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-03-26 17:41:24 -0300

    Rota de criar ONG

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 const { Router } = require('express');
 const routes = Router();
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
-routes.post('/users', (req, res) => {
-  return res.json({
-    evento: 'Semana Omnistack 11.0',
-    aluno: 'Johanny'
+routes.post('/ongs', async (req, res) => {
+
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id, name, email, whatsapp, city, uf
   });
+
+  return res.json({ id });
 });
 
 module.exports = routes;

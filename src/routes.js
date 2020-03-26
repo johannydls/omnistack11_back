a29d1821d279633b098ad55b5100711d973b3d4e
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-03-26 17:43:47 -0300

    Listagem de ONGs

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,11 @@ const routes = Router();
 const crypto = require('crypto');
 const connection = require('./database/connection');
 
+routes.get('/ongs', async (req, res) => {
+  const ongs = await connection.table('ongs').select('*');
+  return res.json(ongs);
+});
+
 routes.post('/ongs', async (req, res) => {
 
   const { name, email, whatsapp, city, uf } = req.body;
